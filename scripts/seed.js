9973fb58487b14b4041063858bfc413928c4343a
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-03-22 15:32:24 -0500

    added .env file

---
+++ scripts/seed.js
@@ -0,0 +1,14 @@
+// const { db } = require("@vercel/postgres")
+
+// const {
+//     clients,
+//     users,
+// } = require("../app/lib/placeholder-date.js")
+
+// const bcrypt = require("bcrypt")
+
+// async function seedUsers(client) {
+//     try {
+//         await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`
+//     }
+// }

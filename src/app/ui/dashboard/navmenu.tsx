8f3c83153f6a6176de34368d92f49d73198b26d9
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-04-19 22:29:16 -0500

    Update navmenu.tsx
    Added a useRef to close menu when user clicks outside of hamburger menu icon

--- src/app/ui/dashboard/navmenu.tsx
+++ src/app/ui/dashboard/navmenu.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Link from "next/link";
 import NavLinks from "./nav-links";
 import { Bars2Icon } from "@heroicons/react/24/outline";
@@ -8,15 +8,25 @@ import { signOut } from "../../../../auth";
 
 export default function NavMenu() {
     const [openNav, setOpenNav] = useState(false)
+    const menuRef = useRef<HTMLElement | null>(null)
+
 
     function handleClick() {
         setOpenNav(prev => !prev)
     }
 
+    const handleClickOutside = (e: MouseEvent) => {
+        if(menuRef.current && !menuRef.current.contains(e.target as Node)) {
+            setOpenNav(false)
+        }
+    }
+
+    document.addEventListener("click", handleClickOutside)
+
     return (
         <div className="flex h-full flex-col md:px-2 ml-auto md:hidden">
             <div className="w-full">
-                <Bars2Icon onClick={handleClick} className="w-10 bg-blue-400 hover:bg-blue-600 text-white p-2 rounded ml-auto mr-2 md:hidden"/>
+                <Bars2Icon onClick={handleClick} ref={menuRef as React.RefObject<SVGSVGElement>} className="w-10 bg-blue-400 hover:bg-blue-600 text-white p-2 rounded ml-auto mr-2 md:hidden"/>
                 <div className={`fixed top-20 right-0 left-0 z-10 flex grow flex-col justify-between ${openNav ? "inline" : "hidden"} md:flex-col space-x-0 space-y-2 md:hidden px-2 bg-white w-full py-2 px-3 rounded`}>
                     <NavLinks />
                     {/* <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div> */}

a003d9c69aed04da2769a3a4cb6e0e34be8b605c
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-04-19 21:55:27 -0500

    Update navmenu.tsx
    changed top position and height of navlinks container

--- src/app/ui/dashboard/navmenu.tsx
+++ src/app/ui/dashboard/navmenu.tsx
@@ -17,7 +17,7 @@ export default function NavMenu() {
         <div className="flex h-full flex-col md:px-2 ml-auto md:hidden">
             <div className="w-full">
                 <Bars2Icon onClick={handleClick} className="w-10 bg-blue-400 hover:bg-blue-600 text-white p-2 rounded ml-auto mr-2 md:hidden"/>
-                <div className={`fixed right-0 left-0 z-10 flex grow flex-col justify-between ${openNav ? "inline" : "hidden"} md:flex-col space-x-0 space-y-2 md:hidden px-2 bg-white w-full h-full py-2 px-3 rounded`}>
+                <div className={`fixed top-20 right-0 left-0 z-10 flex grow flex-col justify-between ${openNav ? "inline" : "hidden"} md:flex-col space-x-0 space-y-2 md:hidden px-2 bg-white w-full py-2 px-3 rounded`}>
                     <NavLinks />
                     {/* <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div> */}
                 </div>

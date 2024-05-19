666de62b8e7442cd2fa59d464615d98ed223deb3
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-05-19 14:21:57 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -23,3 +23,26 @@ self.addEventListener("notificationclick", function(event) {
         clients.openWindow("http://localhost:3000/dashboard")
     )
 })
+
+const cacheName = 'v1'
+
+const cacheClone = async (e) => {
+  const res = await fetch(e.request);
+  const resClone = res.clone();
+
+  const cache = await caches.open(cacheName);
+  await cache.put(e.request, resClone);
+  return res;
+};
+
+const fetchEvent = () => {
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      cacheClone(e)
+        .catch(() => caches.match(e.request))
+        .then((res) => res)
+    );
+  });
+};
+
+fetchEvent();
